9c5d35b10d3d613ec343eb029ad0604d25097eeb
Mabdulah <Mabdulah596@hotmail.com>
2019-02-08 01:42:47 +0500

    abdullah

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+password;
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  login(){
+    if(this.password==""){
+    this.router.navigate([''])
+    }
   }
 
 }
