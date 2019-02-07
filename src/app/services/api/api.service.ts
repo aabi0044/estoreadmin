4b007e9225a02bb91bd74543986d96f820964782
Mabdulah <Mabdulah596@hotmail.com>
2019-02-08 01:19:14 +0500

    abdullah

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor(private afs:AngularFireStore) { }
+  constructor() { }
   //========================== Users=========================
 
 
