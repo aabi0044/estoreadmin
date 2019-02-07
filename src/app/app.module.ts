aee3b3db85125312762a2cec4ccafd4fc1be6a96
aqib feroz <aqibpakii@hotmail.com>
2019-02-08 01:38:43 +0500

    aqib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { environment } from 'src/environments/environment';
+
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// Firestore
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 
+
 const ROUTES = [
   {
     path: '',
-    // component: LoginComponent,
+    component: LoginComponent,
     pathMatch: 'full'
   },
 ]
